90d530b5c55473b67d5875319e5d7f2b4e42a084
arhamathar <arhamathar789@gmail.com>
2021-06-13 12:06:57 +0530

    production ready

--- app.js
+++ app.js
@@ -14,46 +14,57 @@ const usersRoutes = require('./routes/users-route');
 const app = express();
 
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 5000;
 
-app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+app.use(
+    '/uploads/images',
+    express.static(path.join('uploads', 'images'))
+);
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    next();
 });
 
-app.use("/api/places", placesRoutes);
+app.use('/api/places', placesRoutes);
 
-app.use("/api/users", usersRoutes);
+app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
-  throw new HttpError("Route not defined", 404);
+    throw new HttpError('Route not defined', 404);
 });
 
 //a middleware func with 4 arguments is a special function resposible for error handling.
 app.use((error, req, res, next) => {
-  if (req.file) {
-    fs.unlink(req.file.path, (err) => {
-      return next(new err);
-    }); // path is property that exist in file object.
-  }
+    if (req.file) {
+        fs.unlink(req.file.path, (err) => {
+            return next(new err());
+        }); // path is property that exist in file object.
+    }
 
-  if (res.headerSent) {
-    return next(error);
-  }
-  res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occured." })
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({
+        message: error.message || 'An unknown error occured.',
+    });
 });
 
-connectDB().then(() => {
-  app.listen(5000, () => {
-    console.log("Server running at port 5000.")
-  });
-}).catch(err => {
-  console.log(err);
-});
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running at port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -9,253 +9,326 @@ const sendgridTransport = require('nodemailer-sendgrid-transport');
 const HttpError = require('../models/https-error');
 const User = require('../models/user-model');
 
-const transporter = nodemailer.createTransport(sendgridTransport({
-  auth: {
-    api_key: process.env.NODEMAILER_KEY
-  }
-}));
+const transporter = nodemailer.createTransport(
+    sendgridTransport({
+        auth: {
+            api_key: process.env.NODEMAILER_KEY,
+        },
+    })
+);
 
 /*///////////******** Getting all users. ********\\\\\\\\\\\\\ */
 
 const getUsers = async (req, res, next) => {
-  let users;
-  try {
-    users = await User.find({}, "-password"); // everything except password.
-  }
-  catch (error) {
-    return next(new HttpError('Fetching users failed, please try again later.'));
-  }
-  // res.status(200).json({ user: users });
-  res.json({ users: users.map(user => user.toObject({ getters: true })) });
+    let users;
+    try {
+        users = await User.find({}, '-password'); // everything except password.
+    } catch (error) {
+        return next(
+            new HttpError(
+                'Fetching users failed, please try again later.'
+            )
+        );
+    }
+    // res.status(200).json({ user: users });
+    res.json({
+        users: users.map((user) => user.toObject({ getters: true })),
+    });
 };
 
 /*///////////******** Sign Up User. ********\\\\\\\\\\\\\ */
 
 const userSignup = async (req, res, next) => {
-  const error = validationResult(req);
-  if (!error.isEmpty()) {
-    return next(new HttpError('Invalid data entered, please enter data correctly', 422));
-  }
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        return next(
+            new HttpError(
+                'Invalid data entered, please enter data correctly',
+                422
+            )
+        );
+    }
 
-  const { name, email, password } = req.body;
-  let hasUser;
+    const { name, email, password } = req.body;
+    let hasUser;
 
-  try {
-    hasUser = await User.findOne({ email })
-  }
-  catch (error) {
-    return next(new HttpError("Signed Up failed, please try again !", 500));
-  }
+    try {
+        hasUser = await User.findOne({ email });
+    } catch (error) {
+        return next(
+            new HttpError('Signed Up failed, please try again !', 500)
+        );
+    }
 
-  if (hasUser) {
-    return next(new HttpError("Signed Up failed, user already exists.", 422));
-  }
+    if (hasUser) {
+        return next(
+            new HttpError(
+                'Signed Up failed, user already exists.',
+                422
+            )
+        );
+    }
 
-  let hashedPassword;
-  try {
-    hashedPassword = await bcrypt.hash(password, 10);
-  }
-  catch (err) {
-    return next(new HttpError("Could not create user, please try again!", 500));
-  }
+    let hashedPassword;
+    try {
+        hashedPassword = await bcrypt.hash(password, 10);
+    } catch (err) {
+        return next(
+            new HttpError(
+                'Could not create user, please try again!',
+                500
+            )
+        );
+    }
 
-  const newUser = new User({
-    name,
-    email,
-    password: hashedPassword,
-    image: req.file.path,
-    places: []
-  });
-
-  try {
-    await newUser.save();
-  }
-  catch (error) {
-    return next(new HttpError('Creating new user failed, please try again!', 500));
-  }
-
-  let token;
-  try {
-    token = jwt.sign(
-      { userId: newUser.id, email: newUser.email },  // data you want to encode into the token
-      process.env.SECRET_KEY,
-      { expiresIn: '24h' }
-    );
-  }
-  catch (error) {
-    return next(new HttpError("Signing up failed, please try again lataer", 500));
-  }
-
-  try {
-    await transporter.sendMail({
-      to: newUser.email,
-      from: "me.rahulsingh789@gmail.com",
-      subject: "Sign up Successful !",
-      html: "<h1>Welcome to PhotoGram</h1>"
+    const newUser = new User({
+        name,
+        email,
+        password: hashedPassword,
+        image: req.file.path,
+        places: [],
     });
-  } catch (err) {
-    const message = err;
-    return next(new HttpError(message, 500));
-  }
 
-  res.status(201).json({
-    userId: newUser.id,
-    email: newUser.email,
-    token: token
-  });
+    try {
+        await newUser.save();
+    } catch (error) {
+        return next(
+            new HttpError(
+                'Creating new user failed, please try again!',
+                500
+            )
+        );
+    }
+
+    let token;
+    try {
+        token = jwt.sign(
+            { userId: newUser.id, email: newUser.email }, // data you want to encode into the token
+            process.env.SECRET_KEY,
+            { expiresIn: '24h' }
+        );
+    } catch (error) {
+        return next(
+            new HttpError(
+                'Signing up failed, please try again lataer',
+                500
+            )
+        );
+    }
+
+    try {
+        await transporter.sendMail({
+            to: newUser.email,
+            from: 'me.rahulsingh789@gmail.com',
+            subject: 'Sign up Successful !',
+            html: '<h1>Welcome to PhotoGram</h1>',
+        });
+    } catch (err) {
+        const message = err;
+        return next(new HttpError(message, 500));
+    }
+
+    res.status(201).json({
+        userId: newUser.id,
+        email: newUser.email,
+        token: token,
+    });
 }; // newUser.toObject({ getters: true }) it copy _id field to id field.
 
 /*////////////////******** Login User . ********\\\\\\\\\\\\\\\\\\\ */
 
 const userLogin = async (req, res, next) => {
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  let identifiedUser;
-  try {
-    identifiedUser = await User.findOne({ email })
-  }
-  catch (error) {
-    return next(new HttpError("Logging In failed, please try again !", 500));
-  }
+    let identifiedUser;
+    try {
+        identifiedUser = await User.findOne({ email });
+    } catch (error) {
+        return next(
+            new HttpError(
+                'Logging In failed, please try again !',
+                500
+            )
+        );
+    }
 
-  if (!identifiedUser) {
-    return next(new HttpError("User not found , please check email.", 403));
-  }
+    if (!identifiedUser) {
+        return next(
+            new HttpError('User not found , please check email.', 403)
+        );
+    }
 
-  let isValidUser = false;
-  try {
-    isValidUser = await bcrypt.compare(password, identifiedUser.password)
-  }
-  catch (err) {
-    return next(new HttpError("Could not log you in, please check your credentials and try again.", 500));
-  }
+    let isValidUser = false;
+    try {
+        isValidUser = await bcrypt.compare(
+            password,
+            identifiedUser.password
+        );
+    } catch (err) {
+        return next(
+            new HttpError(
+                'Could not log you in, please check your credentials and try again.',
+                500
+            )
+        );
+    }
 
-  if (!isValidUser) {
-    return next(new HttpError("Invalid credentials, please check your password.", 401));
-  }
+    if (!isValidUser) {
+        return next(
+            new HttpError(
+                'Invalid credentials, please check your password.',
+                401
+            )
+        );
+    }
 
-  let token;
-  try {
-    token = jwt.sign(
-      { userId: identifiedUser.id, email: identifiedUser.email },
-      process.env.SECRET_KEY,
-      { expiresIn: '24h' }
-    );
-  }
-  catch (err) {
-    const error = new HttpError(
-      "Logging in failed, please try again later.",
-      500
-    );
-    return next(error);
-  }
+    let token;
+    try {
+        token = jwt.sign(
+            {
+                userId: identifiedUser.id,
+                email: identifiedUser.email,
+            },
+            process.env.SECRET_KEY,
+            { expiresIn: '24h' }
+        );
+    } catch (err) {
+        const error = new HttpError(
+            'Logging in failed, please try again later.',
+            500
+        );
+        return next(error);
+    }
 
-  res.status(200).json({
-    userId: identifiedUser.id,
-    email: identifiedUser.email,
-    token: token
-  });
+    res.status(200).json({
+        userId: identifiedUser.id,
+        email: identifiedUser.email,
+        token: token,
+    });
 };
 
 /*//////////////// ******** Reseting Password ******** \\\\\\\\\\\\\\\\\\\ */
 
 const resetPassword = async (req, res, next) => {
-  crypto.randomBytes(32, async (err, buffer) => {
-    if (err) {
-      // console.log(err);      
-      return next(new HttpError(err, 422));
-    }
+    crypto.randomBytes(32, async (err, buffer) => {
+        if (err) {
+            return next(new HttpError(err, 422));
+        }
 
-    const token = buffer.toString('hex');
+        const token = buffer.toString('hex');
 
-    const email = req.body.email;
-    let user;
-    try {
-      user = await User.findOne({ email });
-    }
-    catch (err) {
-      return next(new HttpError("Resetting password failed, please try again !", 500));
-    }
+        const email = req.body.email;
+        let user;
+        try {
+            user = await User.findOne({ email });
+        } catch (err) {
+            return next(
+                new HttpError(
+                    'Resetting password failed, please try again !',
+                    500
+                )
+            );
+        }
 
-    if (!user) {
-      return next(new HttpError("User not found , please check email.", 403));
-    }
+        if (!user) {
+            return next(
+                new HttpError(
+                    'User not found , please check email.',
+                    403
+                )
+            );
+        }
 
-    user.resetToken = token;
-    user.resetTokenExpiration = Date.now() + 3600000;
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
 
-    try {
-      await user.save();
-    } catch (error) {
-      return next(new HttpError('Resetting password failed, please try again!', 500));
-    }
+        try {
+            await user.save();
+        } catch (error) {
+            return next(
+                new HttpError(
+                    'Resetting password failed, please try again!',
+                    500
+                )
+            );
+        }
 
-    try {
-      await transporter.sendMail({
-        to: user.email,
-        from: "me.rahulsingh789@gmail.com",
-        subject: "Reset Password link !",
-        html: `
+        try {
+            await transporter.sendMail({
+                to: user.email,
+                from: 'me.rahulsingh789@gmail.com',
+                subject: 'Reset Password link !',
+                html: `
             <h4>You're almost ready to reset your password! </h4>
             <p>Click on the link below to reset your password.</p>
             <a href="http://http://localhost:3000/auth/reset-password/${token}">Reset Password</a>
             <p>Cheers,</p>
             <h5>PhotoGram</h5>
-        `
-      });
-    } catch (err) {
-      const message = err;
-      return next(new HttpError(message, 500));
-    }
+        `,
+            });
+        } catch (err) {
+            const message = err;
+            return next(new HttpError(message, 500));
+        }
 
-    res.json({
-      message: "Plese check your email !",
-      resetToken: token
+        res.json({
+            message: 'Plese check your email !',
+            resetToken: token,
+        });
     });
-  });
 };
-
 
 /*//////////////// ******** New Password ******** \\\\\\\\\\\\\\\\\\\ */
 
 const newPassword = async (req, res, next) => {
-  const newPassword = req.body.password;
-  const token = req.body.resetToken;
+    const newPassword = req.body.password;
+    const token = req.body.resetToken;
 
-  let user;
-  try {
-    user = await User.findOne({
-      resetToken: token,
-      resetTokenExpiration: { $gt: Date.now() }
+    let user;
+    try {
+        user = await User.findOne({
+            resetToken: token,
+            resetTokenExpiration: { $gt: Date.now() },
+        });
+    } catch (err) {
+        return next(
+            new HttpError(
+                'Resetting password failed, please try again !',
+                500
+            )
+        );
+    }
+
+    if (!user) {
+        return next(
+            new HttpError('Session expired, please try again !', 422)
+        );
+    }
+
+    let hashedNewPassword;
+    try {
+        hashedNewPassword = await bcrypt.hash(newPassword, 10);
+    } catch (err) {
+        return next(
+            new HttpError(
+                'Could not change password, please try again!',
+                500
+            )
+        );
+    }
+
+    user.password = hashedNewPassword;
+    user.resetToken = undefined;
+    user.resetTokenExpiration = undefined;
+
+    try {
+        await user.save();
+    } catch (err) {
+        return next(new HttpError(err, 422));
+    }
+    res.status(201).json({
+        message: 'Password updated Successfully !',
     });
-  } catch (err) {
-    return next(new HttpError("Resetting password failed, please try again !", 500));
-  }
-
-  if (!user) {
-    return next(new HttpError("Session expired, please try again !", 422));
-  }
-
-  let hashedNewPassword;
-  try {
-    hashedNewPassword = await bcrypt.hash(newPassword, 10);
-  }
-  catch (err) {
-
-    return next(new HttpError("Could not change password, please try again!", 500));
-  }
-
-  user.password = hashedNewPassword;
-  user.resetToken = undefined;
-  user.resetTokenExpiration = undefined;
-
-  try {
-    await user.save()
-  } catch (err) {
-    return next(new HttpError(err, 422));
-  }
-  res.status(201).json({ message: "Password updated Successfully !" });
-
 };
 
 /* ///////////******** Exporting all functions. ********\\\\\\\\\\\\\ */
